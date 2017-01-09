067275b722dd2dc7d0d97a46f9a56134f38ea25c
hannahbishop <hannahbananabishop@gmail.com>
2017-01-08 21:25:53 -0800

    Allowing sass cache

--- config.rb
+++ config.rb
@@ -4,7 +4,6 @@ require 'compass/import-once/activate'
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = ""
-cache = false
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
