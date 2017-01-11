5133a6e5cc5615149dccdd6d3a2796f5604ff9f6
hannahbishop <hannahbananabishop@gmail.com>
2017-01-10 22:02:43 -0800

    Revert "Adding meta boxes"
    This reverts commit 2d6936fe78ed3ab6829539787bdafca66d10aba5.

--- content.php
+++ content.php
@@ -1,16 +1,43 @@
 <?php
 /**
- * The default template for displaying content
+ * The main template file.
  *
- * Used for both single and index/archive/search.
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Write
  */
-?>
 
-	<div class="entry-content">
-		<?php
-			/* translators: %s: Name of current post */
-			the_content();
-		?>
-	</div><!-- .entry-content -->
+get_header(); ?>
 
-</article><!-- #post-## -->
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php the_content(); ?>
+
+			<?php endwhile; ?>
+
+			<?php
+			the_posts_pagination( array(
+				'prev_text' => __( '&laquo; Previous', 'write' ),
+				'next_text' => __( 'Next &raquo;', 'write' ),
+			) );
+			?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'content', 'none' ); ?>
+
+		<?php endif; ?>
+
+		</main>
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
