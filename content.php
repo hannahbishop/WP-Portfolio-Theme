2d6936fe78ed3ab6829539787bdafca66d10aba5
hannahbishop <hannahbananabishop@gmail.com>
2017-01-10 21:47:14 -0800

    Adding meta boxes

--- content.php
+++ content.php
@@ -1,43 +1,16 @@
 <?php
 /**
- * The main template file.
+ * The default template for displaying content
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
- * @package Write
+ * Used for both single and index/archive/search.
  */
+?>
 
-get_header(); ?>
+	<div class="entry-content">
+		<?php
+			/* translators: %s: Name of current post */
+			the_content();
+		?>
+	</div><!-- .entry-content -->
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php the_content(); ?>
-
-			<?php endwhile; ?>
-
-			<?php
-			the_posts_pagination( array(
-				'prev_text' => __( '&laquo; Previous', 'write' ),
-				'next_text' => __( 'Next &raquo;', 'write' ),
-			) );
-			?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'content', 'none' ); ?>
-
-		<?php endif; ?>
-
-		</main>
-	</div><!-- #primary -->
-
-<?php get_footer(); ?>
+</article><!-- #post-## -->
