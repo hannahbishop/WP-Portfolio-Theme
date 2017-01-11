d378060eda41a1631ea3b8a313c91b55f7621674
hannahbishop <hannahbananabishop@gmail.com>
2017-01-10 20:40:19 -0800

    changing content template fetching

--- content.php
+++ content.php
@@ -20,7 +20,7 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'content', get_post_format() ); ?>
+				<?php the_content(); ?>
 
 			<?php endwhile; ?>
 
