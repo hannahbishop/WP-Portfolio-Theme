2c023ebf2860753d08b3a61a4f14c7f985a7d71b
hannahbishop <hannahbishop@w134-87-141-234.wireless.uvic.ca>
2017-01-12 14:32:04 -0800

    Changing WP_Query

--- index.php
+++ index.php
@@ -10,19 +10,9 @@ get_header(); ?>
 
 			<?php $loop = new WP_Query( array( 'post_type' => 'projects', 'posts_per_page' => -1 ) ); ?>
 			<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
-			The stuff you want to loop goes in here
+				<?php the_title();?>
+				<?php get_template_part( 'content', get_post_format() );?>
 			<?php endwhile; wp_reset_query(); ?>
-			/*while ( have_posts() ) : the_post();
-				get_template_part( 'content', get_post_format() );
-			endwhile;
-
-			the_posts_pagination( array(
-				'prev_text'          => __( 'Previous page', 'twentyfifteen' ),
-				'next_text'          => __( 'Next page', 'twentyfifteen' ),
-				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',
-			) );
-			*/
-		?>
 
 		</main><!-- .site-main -->
 	</div><!-- .content-area -->
