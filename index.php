4f0a212c37b78948fa8b42a22abf7c49c72d086c
hannahbishop <hannahbishop@w134-87-141-234.wireless.uvic.ca>
2017-01-12 15:10:08 -0800

    Changing fetched template name

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ get_header(); ?>
 			<?php $loop = new WP_Query( array( 'post_type' => 'projects', 'posts_per_page' => -1 ) ); ?>
 			<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
 				<?php the_title();?>
-				<?php get_template_part( 'content', get_post_format() );?>
+				<?php get_template_part( 'content-projects', get_post_format() );?>
 			<?php endwhile; wp_reset_query(); ?>
 
 		</main><!-- .site-main -->
