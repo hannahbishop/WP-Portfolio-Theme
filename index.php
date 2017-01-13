d63fc6e8349ea63be4de8cda0d39be43615ec86e
hannahbishop <hannahbananabishop@gmail.com>
2017-01-13 11:35:03 -0800

    Removing unneeded title element

--- index.php
+++ index.php
@@ -9,7 +9,6 @@ get_header(); ?>
 
 			<?php $loop = new WP_Query( array( 'post_type' => 'projects', 'posts_per_page' => -1 ) ); ?>
 			<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
-				<?php the_title();?>
 				<?php get_template_part( 'content-project', get_post_format() );?>
 			<?php endwhile; wp_reset_query(); ?>
 
