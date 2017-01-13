ed4f9964adc95e3a56ff286e507175925de788b7
hannahbishop <hannahbananabishop@gmail.com>
2017-01-13 11:27:50 -0800

    Fleshing out project markup

---
+++ content-project.php
@@ -0,0 +1,11 @@
+<div class="project">
+	  <?php
+		$thumb_id = get_post_thumbnail_id();
+		$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
+		$thumb_url = $thumb_url_array[0];
+		?>
+    <a class="feature-img" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" style="background-image:url($thumb_url)"></a>
+		<h3><?php the_title();?></h3>
+		<p><?php the_excerpt();?></p>
+		<a href="<?php the_permalink(); ?>">Process Details</a>
+</div>
--- content-projects.php
+++
@@ -1,7 +0,0 @@
-<div class="project">
-	<?php if ( has_post_thumbnail() ) : ?>
-    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-        <?php the_post_thumbnail(); ?>
-    </a>
-<?php endif; ?>
-</div>
