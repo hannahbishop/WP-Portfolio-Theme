d0fbb554a5d9a893c7ac621c0ea323b3cd3b3da9
hannahbishop <hannahbananabishop@gmail.com>
2017-01-14 11:28:22 -0800

    Changing img link markup

--- content-project.php
+++ content-project.php
@@ -4,7 +4,9 @@
 		$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
 		$thumb_url = $thumb_url_array[0];
 		?>
-    <a class="feature-img" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" style="background-image:url('<?php echo $thumb_url ?>')"></a>
+    <a class="img-link" href="<?php the_permalink();?>" title="<?php the_title();?>">
+				<img class="feature-img" src="<?php echo $thumb_url ?>" alt="<?php the_title();?>">
+		</a>
 		<h3><?php the_title();?></h3>
 		<?php the_excerpt();?>
 		<a href="<?php the_permalink(); ?>">Process Details</a>
