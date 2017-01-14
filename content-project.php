f3e0910d4b0745be17b402d19b2922a644c90797
hannahbishop <hannahbananabishop@gmail.com>
2017-01-14 11:16:28 -0800

    Reverting css directory changes

--- content-project.php
+++ content-project.php
@@ -4,7 +4,7 @@
 		$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
 		$thumb_url = $thumb_url_array[0];
 		?>
-    <a class="feature-img" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" style="background-image:url($thumb_url)"></a>
+    <a class="feature-img" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" style="background-image:url('<?php echo $thumb_url ?>')"></a>
 		<h3><?php the_title();?></h3>
 		<?php the_excerpt();?>
 		<a href="<?php the_permalink(); ?>">Process Details</a>
