123609869b2375f632384d4661532f739743cc67
hannahbishop <hannahbishop@Hannahs-MacBook-Air.local>
2017-01-15 13:20:23 -0800

    Grid markup/styling

--- content-project.php
+++ content-project.php
@@ -1,14 +1,13 @@
-<div class="project">
-	  <?php
-		$thumb_id = get_post_thumbnail_id();
-		$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
-		$thumb_url = $thumb_url_array[0];
-		?>
-    <a class="img-link" href="<?php the_permalink();?>" title="<?php the_title();?>">
-				<img class="feature-img" src="<?php echo $thumb_url ?>" alt="<?php the_title();?>">
-		</a>
-		<div class="project-info">
-			<h3><?php the_title();?></h3>
-			<?php the_excerpt();?>
-		</div>
+<?php
+$thumb_id = get_post_thumbnail_id();
+$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
+$thumb_url = $thumb_url_array[0];
+?>
+
+<div class="project" style="background-image:url(<?php echo $thumb_url ?>)">
+	<!---<img class="feature-img" src="<?php echo $thumb_url ?>" alt="<?php the_title();?>">--->
+	<a class="project-info" href="<?php the_permalink();?>" title="<?php the_title();?>">
+		<h3><?php the_title();?></h3>
+		<?php the_excerpt();?>
+	</a>
 </div>
