82a410f6182786bd245cac18b883c0978bb86cb7
hannahbishop <hannahbishop@Hannahs-MacBook-Air.local>
2017-01-16 13:53:59 -0800

    grid zooming effect

--- content-project.php
+++ content-project.php
@@ -4,8 +4,8 @@ $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true
 $thumb_url = $thumb_url_array[0];
 ?>
 
-<div class="project" style="background-image:url(<?php echo $thumb_url ?>)">
-	<!---<img class="feature-img" src="<?php echo $thumb_url ?>" alt="<?php the_title();?>">--->
+<div class="project">
+	<div class="project-thumbnail" style="background-image:url(<?php echo $thumb_url ?>)"></div><!---using background image here instead of img to allow for better positioning--->
 	<a class="project-info" href="<?php the_permalink();?>" title="<?php the_title();?>">
 		<h3><?php the_title();?></h3>
 		<?php the_excerpt();?>
