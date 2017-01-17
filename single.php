1dba32b39db7ec57280e0a9880e7655e1a42abd8
hannahbishop <hannahbananabishop@gmail.com>
2017-01-17 09:20:08 -0800

    adding loop to contain content

--- single.php
+++ single.php
@@ -4,9 +4,14 @@ get_header(); ?>
 
     <main id="main" class="site-main" role="main">
 
-        <?php the_title()?>
-        <?php the_content()?>
-        <?php echo get_post_meta( get_the_ID(), 'test', true );?>
+        <?php $loop = new WP_Query(
+					array( 'post_type' => 'projects', 'posts_per_page' => -1 ) );
+				?>
+				<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+          <?php the_title();?>
+          <?php the_content();?>
+          <?php echo get_post_meta( get_the_ID(), 'test', true );?>
+				<?php endwhile; wp_reset_query(); ?>
 
     </main><!-- .site-main -->
 
