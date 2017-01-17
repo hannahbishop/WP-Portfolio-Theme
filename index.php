480426fbc4e2ca4ee6451598d4859d01decdb353
hannahbishop <hannahbananabishop@gmail.com>
2017-01-17 08:45:31 -0800

    Revert "changing loop"
    This reverts commit 6f19890217f4977c97bf679c3fc5a63dc009b0e6.

--- index.php
+++ index.php
@@ -8,18 +8,14 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 			<div class="grid">
-				<?php
-				while ( have_posts() ) : the_post();
-
-								/*
-								 * Include the Post-Format-specific template for the content.
-								 * If you want to override this in a child theme, then include a file
-								 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-								 */
-								get_template_part( 'content', get_post_format() );
-
-							// End the loop.
-							endwhile;?>
+				<?php $loop = new WP_Query(
+					array( 'post_type' => 'project', 'posts_per_page' => -1 ) );
+				?>
+				<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+					<div class="grid-item">
+						<?php get_template_part( 'content', get_post_format() );?>
+					</div>
+				<?php endwhile; wp_reset_postdata(); ?>
 			</div>
 
 		</main>
