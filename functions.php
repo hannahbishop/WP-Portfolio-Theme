cc94a6a5687782fe821a421edc500b0e0df0930f
hannahbishop <hannahbananabishop@gmail.com>
2017-01-16 18:47:53 -0800

    adding project post type support

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php
-add_theme_support( 'post-thumbnails' ); 
+add_theme_support( 'post-thumbnails' );
 register_nav_menus( array(
 	'primary'       => 'Header Menu',
 	'secondary'        => 'Footer Menu',
 ) );
+add_post_type_support( 'project', 'post-formats' );
 ?>
