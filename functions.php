29f0a58be51aec29b6577e4f3c8639612b9b02b6
hannahbishop <hannahbananabishop@gmail.com>
2017-01-17 08:45:19 -0800

    Revert "adding setup function"
    This reverts commit 9a24dc735c409088ca98e47ed708331fb7725efc.

--- functions.php
+++ functions.php
@@ -1,11 +1,8 @@
 <?php
-function portfolio_setup() {
-	add_theme_support( 'post-thumbnails' );
-	add_theme_support( 'post-formats', 'project' );
-}
-add_action('after_setup_theme','portfolio_setup');
+add_theme_support( 'post-thumbnails' );
 register_nav_menus( array(
 	'primary'       => 'Header Menu',
 	'secondary'        => 'Footer Menu',
 ) );
+add_post_type_support( 'project', 'post-formats' );
 ?>
