29b6a3d2d377250c69ed9de3e1ec73ddca4e072a
hannahbishop <hannahbananabishop@gmail.com>
2017-01-17 09:45:08 -0800

    removing auto p filter

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php
-add_theme_support( 'post-thumbnails' ); 
+add_theme_support( 'post-thumbnails' );
 register_nav_menus( array(
 	'primary'       => 'Header Menu',
 	'secondary'        => 'Footer Menu',
 ) );
+remove_filter( 'the_content', 'wpautop' );
 ?>
