9d31c7ab1156c535bed96af2d8271ec4960a6fd2
hannahbishop <hannahbananabishop@gmail.com>
2017-01-17 09:35:29 -0800

    changing index loop

--- index.php
+++ index.php
@@ -8,14 +8,11 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 			<div class="grid">
-				<?php $loop = new WP_Query(
-					array( 'post_type' => 'projects', 'posts_per_page' => -1 ) );
-				?>
-				<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+				<?php while ( have_posts() ) : the_post(); ?>
 					<div class="grid-item">
 						<?php get_template_part( 'content-project', get_post_format() );?>
 					</div>
-				<?php endwhile; wp_reset_query(); ?>
+				<?php endwhile; ?>
 			</div>
 
 		</main>
