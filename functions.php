40541acdf3448eee0f7b5913336f51f388f02120
hannahbishop <hannahbananabishop@gmail.com>
2017-01-17 08:46:14 -0800

    Revert "adding project post type support"
    This reverts commit cc94a6a5687782fe821a421edc500b0e0df0930f.

--- functions.php
+++ functions.php
@@ -1,8 +1,7 @@
 <?php
-add_theme_support( 'post-thumbnails' );
+add_theme_support( 'post-thumbnails' ); 
 register_nav_menus( array(
 	'primary'       => 'Header Menu',
 	'secondary'        => 'Footer Menu',
 ) );
-add_post_type_support( 'project', 'post-formats' );
 ?>
