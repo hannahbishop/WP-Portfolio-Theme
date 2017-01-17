9a24dc735c409088ca98e47ed708331fb7725efc
hannahbishop <hannahbananabishop@gmail.com>
2017-01-16 19:33:02 -0800

    adding setup function

--- functions.php
+++ functions.php
@@ -1,8 +1,11 @@
 <?php
-add_theme_support( 'post-thumbnails' );
+function portfolio_setup() {
+	add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'post-formats', 'project' );
+}
+add_action('after_setup_theme','portfolio_setup');
 register_nav_menus( array(
 	'primary'       => 'Header Menu',
 	'secondary'        => 'Footer Menu',
 ) );
-add_post_type_support( 'project', 'post-formats' );
 ?>
