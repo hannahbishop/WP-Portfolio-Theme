dd1125e953d9e8eeefe5e84ae89b8934c7128860
hannahbishop <hannahbananabishop@gmail.com>
2017-01-17 10:45:56 -0800

    single project tsyling

--- content-single.php
+++ content-single.php
@@ -1,5 +1,21 @@
 <div class="project-single">
+  <?php $colour = get_post_meta(get_the_ID(), 'colour');? ?>
   <div class="project-imgs">
     <?php the_content();?>
   </div>
+  <article class="project-info">
+    <h3 style="color: <?php $colour?>"><?php the_title();?></h3>
+    <section>
+      <h4>Problem</h4>
+      <p><?php get_post_meta(get_the_ID(), 'problem'); ?> </p>
+    </section>
+    <section>
+      <h4>Approach</h4>
+      <p><?php get_post_meta(get_the_ID(), 'approach'); ?> </p>
+    </section>
+    <section>
+      <h4>Details</h4>
+      <p><?php get_post_meta(get_the_ID(), 'details'); ?> </p>
+    </section>
+  </article>
 </div>
