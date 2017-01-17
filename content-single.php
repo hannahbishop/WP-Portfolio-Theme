01e592305ecf17e58e5abc88fa36ba5eac08e6b3
hannahbishop <hannahbananabishop@gmail.com>
2017-01-17 12:20:37 -0800

    creating queries for different file types

--- content-single.php
+++ content-single.php
@@ -1,8 +1,35 @@
 <div class="project-single">
   <div class="project-imgs">
-    <?php the_content();?>
+    <?php
+        $query_images_args = array(
+            'post_type'      => 'attachment',
+            'post_mime_type' => 'image,video',// video files include
+            'post_status'    => 'inherit',
+            'orderby'        => 'post_date',
+            'posts_per_page' =>  30,
+        );
+        $query_images = new WP_Query( $query_images_args );
+        if($query_images->have_posts()) :
+            while($query_images->have_posts()) : $query_images->the_post(); ?>
+            <?php echo $images = wp_get_attachment_image( $query_images->posts->ID ); ?>
+            <?php endwhile; ?>
+        <?php wp_reset_postdata(); ?>
   </div>
   <article class="project-details">
+    <?php
+        $query_file_attach = array(
+            'post_type'      => 'attachment',
+            'post_mime_type' => array('application/psd','application/ai', 'application/eps'),
+            'post_status'    => 'inherit',
+            'orderby'        => 'post_date',
+            'posts_per_page' =>  30,
+        );
+        $query_files = new WP_Query( $query_file_attach );
+        if($query_files->have_posts()) :
+            while($query_files->have_posts()) : $query_files->the_post(); ?>
+            <?php the_attachment_link($query_files->posts->ID, false); ?>
+            <?php endwhile; ?>
+        <?php wp_reset_postdata(); wp_reset_querydata(); ?>
     <h3><?php the_title();?></h3>
     <section>
       <h4>Problem</h4>
