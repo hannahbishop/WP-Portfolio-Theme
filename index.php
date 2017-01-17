7231bfda33e0560836ed7f498209f402733fd8c6
hannahbishop <hannahbananabishop@gmail.com>
2017-01-17 09:36:30 -0800

    Revert "changing index loop"
    This reverts commit 9d31c7ab1156c535bed96af2d8271ec4960a6fd2.

--- index.php
+++ index.php
@@ -8,11 +8,14 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 			<div class="grid">
-				<?php while ( have_posts() ) : the_post(); ?>
+				<?php $loop = new WP_Query(
+					array( 'post_type' => 'projects', 'posts_per_page' => -1 ) );
+				?>
+				<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
 					<div class="grid-item">
 						<?php get_template_part( 'content-project', get_post_format() );?>
 					</div>
-				<?php endwhile; ?>
+				<?php endwhile; wp_reset_query(); ?>
 			</div>
 
 		</main>
