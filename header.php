9679d511f8c9677fbfe701a58f09bda91717684a
hannahbishop <hannahbananabishop@gmail.com>
2017-01-19 22:30:18 -0800

    header styling

--- header.php
+++ header.php
@@ -13,12 +13,10 @@
         <header class="site-header">
             <a class="branding" href="http://hannahbishop.com">
                 <h1>Hannah Bishop</h1>
-                <p class="branding-subtitle">Front End Dev, UI/UX</p>
             </a>
             <span class="header-links">
-              <a href="#">Resume</a>
-              <mark>/</mark>
               <a href="mailto:hbishop@uvic.ca">hbishop@uvic.ca</a>
+              <a href="#">Resume</a>
             </span>
             <?php wp_nav_menu( array( 'theme_location' => 'primary' , 'container' => '' , 'menu_class' => 'header-nav') ); ?>
         </header>
