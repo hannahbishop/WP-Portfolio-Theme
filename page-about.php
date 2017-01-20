fe083b6c6f4625e395609deb495fa77681d56fa0
hannahbishop <hannahbishop@w134-87-143-057.wireless.uvic.ca>
2017-01-20 12:46:36 -0800

    adding file

---
+++ page-about.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The main template file
+ */
+
+get_header(); ?>
+
+		<main id="main" class="site-main" role="main">
+
+			<span>Hello World</span>
+
+		</main>
+
+<?php get_footer(); ?>
