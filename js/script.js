3c940d544c76b1a704e2c0171b55aad0003f5e92
hannahbishop <hannahbishop@Hannahs-MacBook-Air.local>
2017-01-22 21:19:30 -0800

    fixing typo

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(function() {
   // Init ScrollMagic Controller
-  var controller = new ScrollMagic();
+  var controller = new ScrollMagic.Controller();
   var scene = new ScrollMagic.Scene({triggerElement: ".site-header"})
   .setPin(".site-header")
   .addIndicators()
