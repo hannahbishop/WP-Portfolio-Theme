c6b7db0540b9260de739e32b09319543696a85af
hannahbishop <hannahbananabishop@gmail.com>
2017-01-23 20:37:42 -0800

    adding grid item loading script

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
 $(function() {
-  // Init ScrollMagic Controller
+  //Sticky header
   var controller = new ScrollMagic();
   var scene = new ScrollScene({triggerElement: ".site-header"})
     .setPin(".site-header")
     .addTo(controller)
     .triggerHook("onLeave");
+
+  //Grid item loading effect
+  var scrollMagicController = new ScrollMagic();
+  var gridLoad = new TimelineMax();
+  gridLoad.add([
+    TweenMax.fromTo('.grid-item', 0.2,{ opacity: 0 }, { opacity: 1 }),
+    TweenMax.fromTo('.grid-item', 0.4,{ top: "10em" }, { top: "0em" })
+  ]);
+   var gridLoadScene = new ScrollScene({
+       triggerElement: '.grid-item',
+   })
+   .setTween(gridLoad)
+   .reverse(false)
+   .triggerHook("onEnter")
+   .addTo(scrollMagicController);
 });
