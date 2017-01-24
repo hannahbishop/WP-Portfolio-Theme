3dff8b076a21ab1e15ea5a36ff8cfe7f2cc82b68
hannahbishop <hannahbananabishop@gmail.com>
2017-01-24 11:45:09 -0800

    Revert "changing masonry init"
    This reverts commit 390a63341d4822b2a1067c5c1470030f41c03e0b.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 $(function() {
 
-  //Masonry
-  var $grid = $('.grid').imagesLoaded( function() {
-    $grid.masonry({
-      itemSelector: '.grid-item',
-      columnWidth: '.grid-sizer',
-      percentPosition: true
-    });
+  //initialize masonry
+  var $grid = $('.grid').masonry({
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-sizer',
+    percentPosition: true
   });
+  // layout Masonry after each image loads
+  $grid.imagesLoaded().progress( function() {
+    $grid.masonry('layout');
+  });
+
 
   //Sticky header
   var controller = new ScrollMagic();
