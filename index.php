3e7108a83365b1acd5d42f0a5331811f5ebb99b7
hannahbishop <hannahbishop@w134-87-138-059.wireless.uvic.ca>
2017-01-24 14:55:11 -0800

    switch class to id

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ get_header(); ?>
 
 		<main id="main" class="site-main" role="main">
 
-			<div class="grid" data-columns>
+			<div id="grid" data-columns>
 				<div class="grid-sizer"></div>
 				<?php $loop = new WP_Query(
 					array( 'post_type' => 'projects', 'posts_per_page' => -1 ) );
