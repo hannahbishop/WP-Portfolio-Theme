a88de99ae14b411b5b45261790264d02f4535c0b
hannahbishop <hannahbishop@w134-87-138-059.wireless.uvic.ca>
2017-01-24 15:02:33 -0800

    changing masonry

--- js/script.js
+++ js/script.js
@@ -1,16 +1,12 @@
 $(function() {
 
-  // init Masonry
-  var $grid = $('.grid').imagesLoaded( function() {
-  // init Masonry after all images have loaded
-    $grid.masonry({
-      itemSelector: '.grid-item',
-      // use element for option
-      columnWidth: '.grid-sizer',
-      percentPosition: true,
-      gutter: 10
-    });
-  });
+  $('.grid').masonry({
+    // set itemSelector so .grid-sizer is not used in layout
+    itemSelector: '.grid-item',
+    // use element for option
+    columnWidth: '.grid-sizer',
+    percentPosition: true
+  })
 
   //Sticky header
   var controller = new ScrollMagic();
