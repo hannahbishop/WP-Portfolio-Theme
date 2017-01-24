22617d4a0fc7a65ac189a8f76f3357968619b5a9
hannahbishop <hannahbishop@w134-87-138-059.wireless.uvic.ca>
2017-01-24 15:10:24 -0800

    fixing typo

--- js/script.js
+++ js/script.js
@@ -1,19 +1,14 @@
 $(function() {
 
-  $(document).ready(function () {
-    var $container = $(".grid");
-
-    $container.imagesLoaded(function () {
-        $container.masonry({
-          // set itemSelector so .grid-sizer is not used in layout
-          itemSelector: '.grid-item',
-          // use element for option
-          columnWidth: '.grid-sizer',
-          percentPosition: true
-        });
-    });
+  var $grid = $('.grid').imagesLoaded( function() {
+  // init Masonry after all images have loaded
+  $grid.masonry({
+    itemSelector: '.grid-item',
+    // use element for option
+    columnWidth: '.grid-sizer',
+    percentPosition: true
   });
-
+});
 
   //Sticky header
   var controller = new ScrollMagic();
