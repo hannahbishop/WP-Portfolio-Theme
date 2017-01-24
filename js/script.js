6bc444ed3a41bfe330db55ed5e247596ad702f76
hannahbishop <hannahbananabishop@gmail.com>
2017-01-23 22:49:24 -0800

    adding grid item loop

--- js/script.js
+++ js/script.js
@@ -9,18 +9,20 @@ $(function() {
   //Grid item scroll loading effect
   //only if images have loaded fully
   $('.project-thumbnail').imagesLoaded( { background: true }, function() {
-    var scrollMagicController = new ScrollMagic();
-    var gridLoad = new TimelineMax();
-    gridLoad.add([
-      TweenMax.fromTo('.grid-item', 0.2,{ opacity: 0 }, { opacity: 1 }),
-      TweenMax.fromTo('.grid-item', 0.4,{ top: "10em" }, { top: "0em" })
-    ]);
-     var gridLoadScene = new ScrollScene({
-         triggerElement: '.grid-item',
-     })
-     .setTween(gridLoad)
-     .reverse(false)
-     .triggerHook("onEnter")
-     .addTo(scrollMagicController);
+    $(".grid-item").each(function (index, elem) {
+      var scrollMagicController = new ScrollMagic();
+      var gridLoad = new TimelineMax();
+      gridLoad.add([
+        TweenMax.fromTo(elem, 0.2,{ opacity: 0 }, { opacity: 1 }),
+        TweenMax.fromTo(elem, 0.4,{ top: "10em" }, { top: "0em" })
+      ]);
+       var gridLoadScene = new ScrollScene({
+           triggerElement: elem,
+       })
+       .setTween(gridLoad)
+       .reverse(false)
+       .triggerHook("onEnter")
+       .addTo(scrollMagicController);
+     });
   });
 });
