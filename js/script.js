390a63341d4822b2a1067c5c1470030f41c03e0b
hannahbishop <hannahbananabishop@gmail.com>
2017-01-24 11:31:34 -0800

    changing masonry init

--- js/script.js
+++ js/script.js
@@ -1,16 +1,13 @@
 $(function() {
 
-  //initialize masonry
-  var $grid = $('.grid').masonry({
-    itemSelector: '.grid-item',
-    columnWidth: '.grid-sizer',
-    percentPosition: true
+  //Masonry
+  var $grid = $('.grid').imagesLoaded( function() {
+    $grid.masonry({
+      itemSelector: '.grid-item',
+      columnWidth: '.grid-sizer',
+      percentPosition: true
+    });
   });
-  // layout Masonry after each image loads
-  $grid.imagesLoaded().progress( function() {
-    $grid.masonry('layout');
-  });
-
 
   //Sticky header
   var controller = new ScrollMagic();
