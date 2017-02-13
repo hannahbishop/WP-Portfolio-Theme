fa5cf01b35e275a1f24c90d42ff5df1ce98f080a
hannahbishop <hannahbishop@Hannahs-MacBook-Air.local>
2017-02-13 11:31:02 -0800

    finishing main landing page styling

--- front-page.php
+++ front-page.php
@@ -6,26 +6,32 @@
 get_header(); ?>
 
 		<main class="site-main">
-      <div class="landing-page">
+      <div class="landing-page" style="background-image: url(<?php bloginfo('template_directory');?>/assets/img/swing.jpg);">
 				<div class="hero">
+					<?php get_template_part( 'assets/svg/letterhead.svg' );?>
 	        <h1>Hannah Bishop</h1>
 	        <ul class="intro-txt">
 	          <li><p>Front end designer and developer.</p></li>
 	          <li><p>Computer science student at UVic.</p></li>
 	          <li><p>Cat owner.</p></li>
 	        </ul>
-	        <a class="cta" href="#">Email me</a>
-	        <a class="cta-secondary" href="#">or view my resume</a>
+					<div class="cta">
+	        	<a class="cta-btn" href="mailto:hbishop@uvic.ca">
+							<span class="cta-btn-txt" data-hover="Email me">Email me</span>
+						</a>
+	        	<a class="txt-link" href="#">view resume</a>
+					</div>
 				</div>
 	      <div class="contact">
-	        <ul>
-	          <li><a class="contact-link" href="https://github.com/hannahbishop">
+	        <ul class="icon-list">
+	          <li><a class="link-icon" href="https://github.com/hannahbishop" title="GitHub">
 							<?php get_template_part( 'assets/svg/icon', 'github.svg' );?>
 						</a></li>
-	          <li><a class="contact-link" href="https://www.instagram.com/littlehannahbee/">
+	          <li><a class="link-icon" href="https://www.instagram.com/littlehannahbee/" title="Instagram">
 							<?php get_template_part( 'assets/svg/icon', 'instagram.svg' );?>
 						</a></li>
 	          </ul>
+						<a class="txt-link" href="mailto:hbishop@uvic.ca">hbishop@uvic.ca</a>
         </div>
       </div>
 		</main>
--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 
     <head>
         <title>Hannah Bishop</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/style.css">
         <?php wp_head();?>
     </head>
