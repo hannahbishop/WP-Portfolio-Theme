6aa7cb966a054546f32c277a0737016692649e72
hannahbishop <hannahbishop@Hannahs-MacBook-Air.local>
2017-02-12 18:50:22 -0800

    fixing flexbox styling

--- front-page.php
+++ front-page.php
@@ -7,23 +7,25 @@ get_header(); ?>
 
 		<main class="site-main">
       <div class="landing-page">
-        <h1>Hannah Bishop</h1>
-        <ul class="intro-txt">
-          <li><p>Front end designer and developer.</p></li>
-          <li><p>Computer science student at UVic.</p></li>
-          <li><p>Cat owner.</p></li>
-        </ul>
-        <a class="cta" href="#">Email me</a>
-        <a class="cta-secondary" href="#">or view my resume</a>
-        <div class="contact">
-          <ul>
-            <li><a href="https://github.com/hannahbishop">
+				<div class="hero">
+	        <h1>Hannah Bishop</h1>
+	        <ul class="intro-txt">
+	          <li><p>Front end designer and developer.</p></li>
+	          <li><p>Computer science student at UVic.</p></li>
+	          <li><p>Cat owner.</p></li>
+	        </ul>
+	        <a class="cta" href="#">Email me</a>
+	        <a class="cta-secondary" href="#">or view my resume</a>
+				</div>
+	      <div class="contact">
+	        <ul>
+	          <li><a class="contact-link" href="https://github.com/hannahbishop">
 							<?php get_template_part( 'assets/svg/icon', 'github.svg' );?>
 						</a></li>
-            <li><a href="https://www.instagram.com/littlehannahbee/">
+	          <li><a class="contact-link" href="https://www.instagram.com/littlehannahbee/">
 							<?php get_template_part( 'assets/svg/icon', 'instagram.svg' );?>
 						</a></li>
-          </ul>
+	          </ul>
         </div>
       </div>
 		</main>
